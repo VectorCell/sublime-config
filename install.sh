2877f5b8e6f03a4a894f0ff780b86ff5fcbd0838
Brandon Smith <vectorcell@gmail.com>
2016-08-01 12:34:06 -0500

    Added install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ ! -e "~/.config/sublime-text-3/Installed Packages/Theme - Nexus.sublime-package-backup" ]; then
+	mv "~/.config/sublime-text-3/Installed Packages/Theme - Nexus.sublime-package" "~/.config/sublime-text-3/Installed Packages/Theme - Nexus.sublime-package-backup"
+fi
+cp "Theme - Nexus.sublime-package" "~/.config/sublime-text-3/Installed Packages/"
+
