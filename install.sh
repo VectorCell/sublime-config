31fcfc256876783ab5cd41a5111ae5e7f442cec3
Brandon Smith <vectorcell@gmail.com>
2016-08-01 12:36:00 -0500

    fixed install script

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 if [ ! -e "~/.config/sublime-text-3/Installed Packages/Theme - Nexus.sublime-package-backup" ]; then
 	mv "~/.config/sublime-text-3/Installed Packages/Theme - Nexus.sublime-package" "~/.config/sublime-text-3/Installed Packages/Theme - Nexus.sublime-package-backup"
 fi
-cp "Theme - Nexus.sublime-package" "~/.config/sublime-text-3/Installed Packages/"
+cp "Theme - Nexus.sublime-package" ~/.config/sublime-text-3/Installed\ Packages/
 
